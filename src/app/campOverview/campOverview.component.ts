83679f6239ebc9283347d22f187938665fbb0606
Sandro Burkhart <sandroburkhart@sandros-27imac.home>
2019-12-05 22:46:57 +0100

    campoverview eingebaut.

---
+++ src/app/campOverview/campOverview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-campOverview',
+  templateUrl: './campOverview.component.html',
+  styleUrls: ['./campOverview.component.css']
+})
+export class CampOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
