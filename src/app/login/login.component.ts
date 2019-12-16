c21acd79843a53a3874eb72515710139f31c7e81
Sandro Burkhart <sandroburkhart@sandros-27imac.home>
2019-12-16 22:37:52 +0100

    änderung xy

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+
+    console.log("jhjg");
+  }
 
   ngOnInit() {
   }
